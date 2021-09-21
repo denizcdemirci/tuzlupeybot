026f51564dfc2d9e7903b877a79dde920ebc7f4a
Deniz Çağın <denizcdemirci@gmail.com>
2021-09-21 11:10:30 +0300

    block some voice channels

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,6 +1,14 @@
 module.exports = (client, oldState, newState) => {
+  const voiceChannels = [
+    '107541160855105536',
+    '152775281055236097',
+    '789546252101484544',
+    '818258673171824640',
+  ];
+
   if (!newState.channel) return;
   if (newState.member.user.bot) return;
+  if (voiceChannels.some((channel) => channel === newState.channelID)) return;
 
   client.user.setActivity(newState.channel.name.substr(newState.channel.name.indexOf(' ') + 1));
 };
