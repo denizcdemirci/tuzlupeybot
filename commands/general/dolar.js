c9aec6bdc8ac34eecee9ee3bbbed49ec34b2b31f
Celal Deniz <34776010+fekajin@users.noreply.github.com>
2021-10-25 21:54:14 +0300

    rjz4 update

--- commands/general/dolar.js
+++ commands/general/dolar.js
@@ -5,17 +5,16 @@ module.exports = {
     utilisation: '{prefix}dolar',
     execute(client, message) {
         const fetch = require('node-fetch');
-
-
         fetch(`https://freecurrencyapi.net/api/v2/latest?apikey=b059a8b0-35bf-11ec-990f-e122c752a66e&base_currency=USD`)
             .then((response) => {
                 return response.json();
             }).then(({ data }) => {
                 const { TRY } = data;
-                message.channel.send(`Dolar şuan ${TRY} civarında oynaşmakta. Denizin istediği macbook ise bir sonraki fiyat güncellemesinde ${2325 * TRY} TL olacak. :Sadge:`);
+                message.channel.send(`Dolar şuan ${TRY.toFixed(2)} civarında oynaşmakta. 
+              Denizin istediği macbook ise bir sonraki fiyat güncellemesinde ${(2516 * TRY).toFixed(2)} TL olacak. <:Sadge:834156037279973386> `);
             }).catch(err => {
-                message.reply(err?.status == 429 ? 'Maalesef bu aylık istek limitlerini aştık. GElecek ay görüşmek üzere :rjza:' :
-                    'Bilinmeyen bir hata oluştu. Bilgi almak için @lil deniz#0001 adlı kullanıcıya ulaşabilirsiniz.');
+                message.reply(err?.status == 429 ? 'Maalesef bu aylık istek limitlerini aştık. Gelecek ay görüşmek üzere <:rjza:743900789717598308>' :
+                    'Bilinmeyen bir hata oluştu. Bilgi almak için <@102835660141916160> adlı kullanıcıya ulaşabilirsiniz.');
             });
     },
 };
