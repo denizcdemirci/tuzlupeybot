ed9d34e6553f6d660a13808f165a36032afb2aef
Deniz Çağın <denizcdemirci@gmail.com>
2023-03-03 14:39:28 +0300

    Update dolor command

--- commands/general/dolar.js
+++ commands/general/dolar.js
@@ -3,40 +3,34 @@ module.exports = {
   aliases: [],
   category: 'General',
   utilisation: '{prefix}dolar',
-    execute(client, message) {
+  execute(client, message) {
     const fetch = require('node-fetch');
 
-   const headers = { apikey: "K8707gqiH3gnFgRvcKFy2pBESDp9G8Nf" };
+    fetch('https://api.apilayer.com/exchangerates_data/latest?base=usd', {
+      method: 'GET',
+      headers: {
+        apikey: 'K8707gqiH3gnFgRvcKFy2pBESDp9G8Nf'
+      },
+    }).then((response) => {
+      return response.json();
+    }).then(({rates}) => {
+      const { TRY } = rates;
 
-fetch("https://api.apilayer.com/exchangerates_data/latest?base=usd", {
-  method: "GET",
-  headers: headers,
-})
-  .then((response) => {
-    console.log("first", response);
-    return response.json();
-  })
-  .then(({ rates }) => {
-    const { TRY } = rates;
-
-    message.channel.send(
-      `Dolar şuan ${TRY.toFixed(
-        2
-      )} TL civarında oynaşmakta.\nDenizin istediği iphone 14 pro ise bir sonraki fiyat güncellemesinde ${(
-        2194.12 * TRY
-      ).toFixed(2)} TL olacak. Zeta'nın gitmek istediği yurtdışı seyahatinin masrafları bir sonraki fiyat güncellemesinde ${(1500*TRY).toFixed(2)} TL olacak. <:Sadge:834156037279973386>`
-    );
-  })
-  .catch((e) => {
-    if (e.status === 429) {
-      message.reply(
-        "Maalesef bu aylık istek limitlerini aştık. Gelecek ay görüşmek üzere <:rjza:743900789717598308>"
+      message.channel.send(
+        `Dolar şuan ${
+          TRY.toFixed(2)
+        } TL civarında oynaşmakta.\nDenizin istediği iphone 14 pro ise bir sonraki fiyat güncellemesinde ${
+          (2194.12 * TRY).toFixed(2)
+        } TL olacak. Zeta'nın gitmek istediği yurtdışı seyahatinin masrafları bir sonraki fiyat güncellemesinde ${
+          (1500 * TRY).toFixed(2)
+        } TL olacak. <:Sadge:834156037279973386>`
       );
-    } else {
-      message.reply(
-        "Bilinmeyen bir hata oluştu. Bilgi almak için <@102835660141916160> adlı kullanıcıya ulaşabilirsiniz."
-      );
-    }
-  });
+    }).catch((e) => {
+      if (e.status === 429) {
+        message.reply('Maalesef bu aylık istek limitlerini aştık. Gelecek ay görüşmek üzere <:rjza:743900789717598308>');
+      } else {
+        message.reply('Bilinmeyen bir hata oluştu. Bilgi almak için <@102835660141916160> adlı kullanıcıya ulaşabilirsiniz.');
+      }
+    });
   },
 };
