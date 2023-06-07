d24ce8aea1ffa86c050ed237302a2d8afb9f75e1
deniz çağın <denizcdemirci@gmail.com>
2023-06-07 13:37:29 +0300

    Update dolar.js

--- commands/general/dolar.js
+++ commands/general/dolar.js
@@ -18,10 +18,8 @@ module.exports = {
       inter.reply(
         `Dolar şuan ${
           TRY.toFixed(2)
-        } TL civarında oynaşmakta.\nDenizin istediği iphone 14 pro ise bir sonraki fiyat güncellemesinde ${
+        } TL civarında oynaşmakta.\niPhone 14 Pro 128GB bir sonraki fiyat güncellemesinde ${
           (2194.12 * TRY).toFixed(2)
-        } TL olacak. Zeta'nın gitmek istediği yurtdışı seyahatinin masrafları bir sonraki fiyat güncellemesinde ${
-          (1500 * TRY).toFixed(2)
         } TL olacak. <:Sadge:834156037279973386>`
       );
     } catch (error) {
